989d61660a1d23cdbde1c4ffb73833c69beac00f
Margon  <monondcosta@gmail.com>
2026-04-05 20:10:33 +0600

    Add: Added prisma service.

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { BibleModule } from './bible/bible.module';
+import { PrismaService } from './prisma/prisma.service';
 
 @Module({
   imports: [BibleModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, PrismaService],
 })
 export class AppModule {}
