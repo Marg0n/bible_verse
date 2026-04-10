60709d07b8dc335c5e803bfdfdad56fcbc33be13
Margon  <monondcosta@gmail.com>
2026-04-10 23:19:49 +0600

    Add: Added user seervice.

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,26 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(private prisma: PrismaService) {}
+
+  createUser() {
+    return this.prisma.user.create({
+      data: {},
+    });
+  }
+
+  getUser(id: string) {
+    return this.prisma.user.findUnique({
+      where: { id },
+    });
+  }
+
+  updateTheme(userId: string, theme: string) {
+    return this.prisma.user.update({
+      where: { id: userId },
+      data: { theme },
+    });
+  }
+}
