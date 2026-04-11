d1099ca05e14b6c7f730a9a4a2a9de8efc683cfa
Margon  <monondcosta@gmail.com>
2026-04-11 22:35:02 +0600

    Modify/Add: Modified prisma schema and user service. Added user controller.

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,23 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { UserService } from './user.service';
+import { Theme } from '@prisma/client';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+  constructor(private userService: UserService) {}
+
+  @Post()
+  createUser() {
+    return this.userService.createUser();
+  }
+
+  @Get()
+  getUser(@Param('id') id: string) {
+    return this.userService.getUser(id);
+  }
+
+  @Post(':id/theme')
+  updateTheme(@Param('id') id: string, @Body('theme') theme: Theme) {
+    return this.userService.updateTheme(id, theme);
+  }
+}
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
+import { Theme } from '@prisma/client';
 
 @Injectable()
 export class UserService {
@@ -14,10 +15,11 @@ export class UserService {
   getUser(id: string) {
     return this.prisma.user.findUnique({
       where: { id },
+      include: { favorites: true },
     });
   }
 
-  updateTheme(userId: string, theme: string) {
+  updateTheme(userId: string, theme: Theme) {
     return this.prisma.user.update({
       where: { id: userId },
       data: { theme },
