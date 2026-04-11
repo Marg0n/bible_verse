3b5f1bb6164699092bef6ee2504ea658065f2e00
Margon  <monondcosta@gmail.com>
2026-04-12 02:37:55 +0600

    Add: Added favorite controller, module & service.

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,10 @@ import { BibleModule } from './bible/bible.module';
 import { PrismaService } from './prisma/prisma.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { UserModule } from './user/user.module';
+import { FavoritesModule } from './favorites/favorites.module';
 
 @Module({
-  imports: [BibleModule, PrismaModule, UserModule],
+  imports: [BibleModule, PrismaModule, UserModule, FavoritesModule],
   controllers: [AppController],
   providers: [AppService, PrismaService],
 })
