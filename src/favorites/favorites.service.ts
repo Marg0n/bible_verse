774c807b1ce2e35635e143b99ace28204a3123b1
Margon  <monondcosta@gmail.com>
2026-04-13 22:04:27 +0600

    Add: Added favorite services functions.

--- src/favorites/favorites.service.ts
+++ src/favorites/favorites.service.ts
@@ -1,4 +1,35 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
 
 @Injectable()
-export class FavoritesService {}
+export class FavoritesService {
+  constructor(private prisma: PrismaService) {}
+
+  addFavorites(userId: string, verseId: string) {
+    return this.prisma.favorite.create({
+      data: {
+        userId,
+        verseId,
+      },
+    });
+  }
+
+  getFavorites(userId: string) {
+    return this.prisma.favorite.findMany({
+      where: {
+        userId,
+      },
+    });
+  }
+
+  removeFavorites(userId: string, verseId: string) {
+    return this.prisma.favorite.delete({
+      where: {
+        userId_verseId: {
+          userId,
+          verseId,
+        },
+      },
+    });
+  }
+}
