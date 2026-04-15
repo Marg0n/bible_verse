8040fb1fb34c93b97bba90a889a4be74b69c85fc
Margon  <monondcosta@gmail.com>
2026-04-15 11:40:48 +0600

    Add: Added streak get method controller.

--- src/streak/streak.controller.ts
+++ src/streak/streak.controller.ts
@@ -1,4 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get } from '@nestjs/common';
+import { StreakService } from './streak.service';
 
 @Controller('streak')
-export class StreakController {}
+export class StreakController {
+  constructor(private readonly streakService: StreakService) {}
+
+  @Get()
+  updateStreak(@Body('userId') userId: string) {
+    if (!userId) {
+      return { message: 'userId is required' };
+    }
+
+    return this.streakService.updateStreak(userId);
+  }
+}
