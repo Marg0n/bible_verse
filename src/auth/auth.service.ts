54335ca4a2e2738933af79b2d71aaf9efe1d052c
Margon  <monondcosta@gmail.com>
2026-04-23 11:30:39 +0600

    Add: Added createAnonymousUser in auth service.

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,36 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { PrismaService } from '../prisma/prisma.service';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    private readonly jwtservice: JwtService,
+    private readonly prisma: PrismaService,
+  ) {}
+
+  async createAnonymousUser() {
+    try {
+      const user = await this.prisma.user.create({
+        data: {},
+      });
+
+      const token = this.jwtservice.sign({
+        sub: user.id, //? sub = subject (standard JWT field))
+      });
+
+      const result = {
+        user,
+        access_token: token,
+      };
+
+      return {
+        success: true,
+        data: result,
+      };
+    } catch (error) {
+      console.error('createAnonymousUser: ', error);
+      throw new BadRequestException('User Creation failed');
+    }
+  }
+}
