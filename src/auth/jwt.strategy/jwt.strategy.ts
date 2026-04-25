1b68a8be4f26cad65196507b8418af9cb2afc7aa
Margon  <monondcosta@gmail.com>
2026-04-25 19:57:24 +0600

    Add: Added auth/jwt.strategy.

---
+++ src/auth/jwt.strategy/jwt.strategy.ts
@@ -0,0 +1,18 @@
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor() {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      secretOrKey: 'JWT_SECRET',
+    });
+  }
+
+  async validate(payload: any) {
+    const userData = await payload?.sub;
+    return { userId: userData };
+  }
+}
