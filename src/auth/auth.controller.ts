e8ae3563dac7a06d8002cb0ef2c940fa158db50b
Margon  <monondcosta@gmail.com>
2026-04-29 15:45:05 +0600

    Modify: url for auth create user modified.

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -5,7 +5,7 @@ import { AuthService } from './auth.service';
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
-  @Post('anonymous')
+  @Post('user')
   createAnonymous() {
     return this.authService.createAnonymousUser();
   }
