28be4f0c992db160562d58b68a4a2e8079dde6a6
Margon  <monondcosta@gmail.com>
2026-07-04 14:12:43 +0600

    Modify: Modified ThrottlerFilter function to warn route & ip.

--- src/common/filters/throttler.filter.ts
+++ src/common/filters/throttler.filter.ts
@@ -1,7 +1,7 @@
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
 /* eslint-disable @typescript-eslint/no-unsafe-call */
-import { Catch, ExceptionFilter, ArgumentsHost } from '@nestjs/common';
+import { Catch, ExceptionFilter, ArgumentsHost, Logger } from '@nestjs/common';
 import { ThrottlerException } from '@nestjs/throttler';
 
 @Catch(ThrottlerException)
@@ -9,6 +9,18 @@ export class ThrottlerFilter implements ExceptionFilter {
   catch(exception: ThrottlerException, host: ArgumentsHost) {
     const response = host.switchToHttp().getResponse();
 
+    //? Log the blocked attacks
+    /**
+     * example logs:
+     * WARN Rate limit exceeded
+     * IP: 103.xxx.xxx.xxx
+     * Route: POST /auth/login
+     */
+    const request = host.switchToHttp().getRequest();
+
+    Logger.warn(`Rate limit exceeded: ${request.ip}`, 'RateLimiter');
+
+    //? Send status
     response.status(429).json({
       success: false,
       statusCode: 429,
