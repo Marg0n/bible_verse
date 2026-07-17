bc893692cd8457b81e60aac62b543ef665d0638d
Margon  <monondcosta@gmail.com>
2026-07-17 20:22:20 +0600

    Add/Modify: Added app.config and Modified env example.

---
+++ src/config/app.config.ts
@@ -0,0 +1,10 @@
+import { registerAs } from '@nestjs/config';
+
+export default registerAs('app', () => ({
+  port: Number(process.env.PORT) || 3000,
+  nodeEnv: process.env.NODE_ENV ?? 'development',
+  baseUrl: process.env.BASE_URL,
+  allowedOrigins:
+    process.env.ALLOWED_ORIGINS?.split(',').map((origin) => origin.trim()) ??
+    [],
+}));
