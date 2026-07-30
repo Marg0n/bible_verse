cc163f338f0b08f23e0e8c558925423d0324f306
Margon  <monondcosta@gmail.com>
2026-07-30 14:04:12 +0600

    Add: Added MailService Skeleton, Transporter & Verify SMTP Connection.

--- src/mail/mail.service.ts
+++ src/mail/mail.service.ts
@@ -1,6 +1,47 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger, OnModuleInit } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import * as nodemailer from 'nodemailer';
+import { MailOptions } from 'nodemailer/lib/json-transport';
 
 @Injectable()
-export class MailService {
-  //TODO: As the project grows, if email functionality expands (welcome emails, newsletters, verification, notifications), we can later promote mail into its own top-level module.
+export class MailService implements OnModuleInit {
+  //? For logging
+  private readonly logger = new Logger(MailService.name);
+
+  //? Transporter type
+  private transporter!: nodemailer.Transporter;
+
+  //? Constructor
+  constructor(private readonly configService: ConfigService) {}
+
+  //? This runs automatically when the app starts
+  async onModuleInit() {
+    //? Create Transporter
+    this.transporter = nodemailer.createTransport({
+      host: this.configService.getOrThrow<string>('mail.host'),
+      port: this.configService.getOrThrow<number>('mail.port'),
+      secure: false,
+      auth: {
+        user: this.configService.getOrThrow<string>('mail.user'),
+        pass: this.configService.getOrThrow<string>('mail.password'),
+      },
+    });
+
+    //? Verify SMTP Connection
+    try {
+      await this.transporter.verify();
+
+      this.logger.log('SMTP connection established');
+    } catch (error: any) {
+      this.logger.error(
+        'SMTP connection failed',
+        error instanceof Error ? error.stack : undefined,
+      );
+    }
+  }
+
+  async sendMail(options: MailOptions) {
+    //
+  }
+  //TODO: As the project grows, if email functionality expands (welcome emails, newsletters, verification, notifications).
 }
