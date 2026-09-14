8f735f6c6db07b94d4f9bdd255313ee5d7854491
Margon  <monondcosta@gmail.com>
2026-09-14 15:58:39 +0600

    Modify: Modified favorites.service.spec.ts to do mock test.

--- src/favorites/favorites.service.spec.ts
+++ src/favorites/favorites.service.spec.ts
@@ -1,12 +1,27 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { FavoritesService } from './favorites.service';
+import { PrismaService } from '../prisma/prisma.service';
+import { BibleService } from '../bible/bible.service';
 
 describe('FavoritesService', () => {
   let service: FavoritesService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [FavoritesService],
+      providers: [
+        FavoritesService,
+        //? Mock PrismaService
+        {
+          provide: PrismaService,
+          useValue: {},
+        },
+
+        //? Mock BibleService
+        {
+          provide: BibleService,
+          useValue: {},
+        },
+      ],
     }).compile();
 
     service = module.get<FavoritesService>(FavoritesService);
